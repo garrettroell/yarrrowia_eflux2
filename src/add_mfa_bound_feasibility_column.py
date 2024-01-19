f073e5d7ac0b7e44643aec353a35452cebd0c4a3
Garrett Roell <garrettroell@gmail.com>
2024-01-18 23:33:23 -1000

    updated biomass equation and notebook B bound calculations

--- src/add_mfa_bound_feasibility_column.py
+++ src/add_mfa_bound_feasibility_column.py
@@ -3,12 +3,12 @@ from check_feasibility import check_feasibility
 
 def add_mfa_bound_feasibility_column(full_central_rxn_df, substrate):
     mfa_bound_feasibilities = []
-    gsm_lb, gsm_ub = f'{substrate}_GSM_LB', f'{substrate}_GSM_UB'
-    mfa_lb, mfa_ub = f'{substrate}_LB', f'{substrate}_UB'
+    gsm_lb, gsm_ub = f'{substrate} GSM LB', f'{substrate} GSM UB'
+    mfa_lb, mfa_ub = f'{substrate} MFA LB', f'{substrate} MFA UB'
     feasibility_column = f'{substrate}_mfa_bound_feasibility'
 
     # loop over flux rows
-    for index, row in full_central_rxn_df.iterrows():
+    for _, row in full_central_rxn_df.iterrows():
         # get the MFA and GSM flux bounds
         gsm_flux_bounds = [row[gsm_lb], row[gsm_ub]]
         mfa_flux_bounds = [row[mfa_lb], row[mfa_ub]]
